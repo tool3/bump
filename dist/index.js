a9becca881d486b13df3293136a9282a238d22a7
Tal Hayut <tal.hayut@zerto.com>
2019-12-30 22:20:48 +0200

    version dry run

--- dist/index.js
+++ dist/index.js
@@ -1294,9 +1294,10 @@ async function run() {
     const email = core.getInput('email');
     const token = core.getInput('github_token');
 
-    const package = JSON.stringify(exec.exec('npm view version .'));
+    const package = JSON.stringify(exec('npm view version .'));
     const version = package.version;
     const context = github.context;
+    
     const { repository: { git_url }, sender: { login } } = github.context.payload;
     exec('git', ['pull', 'origin', 'master', '--tags']);
     exec('npm', ['version', '--no-commit-hooks', 'patch', '--dry-run']);
