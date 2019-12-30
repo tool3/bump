aea8fb3ad42d1d8c7a61641a799fd32be48781bc
Tal Hayut <tal.hayut@zerto.com>
2019-12-30 20:38:13 +0200

    starting with context

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-
+const github = require('@actions/github');
 async function run() {
   try { 
     const user = core.getInput('user');
     const email = core.getInput('email');
     const token = core.getInput('github_token');
 
-    exec.exec('git pull origin master --tags');
-    const ver = exec.exec('npm view version .');
-    const version = JSON.stringify(ver).version;
-    exec.exec('npm version --no-commit-hooks patch --dry-run');
-    
+    // exec.exec('git pull origin master --tags');
+    // const ver = exec.exec('npm view version .');
+    // const version = JSON.stringify(ver).version;
+    // exec.exec('npm version --no-commit-hooks patch --dry-run');
+    const context = github.context;
+    console.log(context);
+    core.setOutput(`Context:\n${context}`);
     core.setOutput(version);
-    core.setOutput(user);
-    core.setOutput(email);
-    core.setOutput(token);
+  
   } 
   catch (error) {
     core.setFailed(error.message);
