7511ed68ccc3fe054692664f5485706464cfeb8a
Tal Hayut <tal.hayut@zerto.com>
2019-12-31 08:11:44 +0200

    strategy should be #minor

--- index.js
+++ index.js
@@ -12,20 +12,30 @@ const STRATEGIES = [
 Toolkit.run(async tools => {
   {
     try {
-
+      // get input credentials
+      const inputUser = core.getInput('user');
+      const inputEmail = core.getInput('email');
+      const inputBranch = core.getInput('branch');
+      // get current repo
       const pkg = tools.getPackageJSON();
       const event = tools.context.payload;
       const context = github.context;
 
       const { repository: { git_url }, sender: { login }, pusher: { email, name }, head_commit: { message } } = github.context.payload;
 
-      const strategy = STRATEGIES.filter(strat => message.includes(strat))[0] || "#patch";
+      const strategy = STRATEGIES.filter(strat => message.includes(strat))[0].replace('#', '') || "patch";
       console.log('strategy is: ', strategy);
 
-      exec('git', ['config', '--local', 'user.name', name]);
-      exec('git', ['config', '--local', 'user.email', email]);
+      // git login and pull
+      exec('git', ['config', '--local', 'user.name', inputUser || name]);
+      exec('git', ['config', '--local', 'user.email', inputEmail || email]);
       exec('git', ['pull', 'origin', 'master', '--tags']);
-      exec('npm', ['version', '--no-commit-hooks', strategy.replace('#', ''), '--dry-run']);
+
+      // version by strategy
+      exec('npm', ['version', '--no-commit-hooks', strategy, '--dry-run']);
+      // /git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION;
+
+      exec('git', ['push', 'origin', `HEAD:${branch}`, '--tags', '--dry-run'])
 
       console.log(pkg.version);
       console.log(event);
